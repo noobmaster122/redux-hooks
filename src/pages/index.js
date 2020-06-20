1a6f562bd6d46348ded74c827f8221ee91223e9e
noobmaster122 <brahimi.a.farouk@gmail.com>
2020-06-21 00:51:15 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { UserContext } from "../UserContext";
+//import { login } from "../utils/login";
+
+export function Index() {
+  const { user, setUser } = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>Home</h2>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      {user ? (
+        <button
+          onClick={() => {
+            // call logout
+            setUser(null);
+          }}
+        >
+          logout
+        </button>
+      ) : (
+        <button
+          onClick={async () => {
+         //   const user = await login();
+         const userr= "ahmed";
+            setUser(userr);
+          }}
+        >
+          login
+        </button>
+      )}
+    </div>
+  );
+}
